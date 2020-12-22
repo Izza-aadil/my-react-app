4e0d1535f7a6688301dcab64e1b7428fe0cd1c15
Izza Aadil <izzaimran95@gmail.com>
2020-12-23 00:22:39 +0100

    added bootstrap and footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello </div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App!</h1>
+        <footer>
+          <a href="https://github.com/Izza-aadil/My-cool-app">
+            {" "}
+            Open-source Code{" "}
+          </a>{" "}
+          by Izza Aadil
+        </footer>{" "}
+      </div>
+    </div>
+  );
 }
 
 export default App;
